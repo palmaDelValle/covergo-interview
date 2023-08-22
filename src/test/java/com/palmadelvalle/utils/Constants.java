c398f3d20c6a0b6bbfc9fe0fa239b196a569c07f
Francisco Javier Palma Del Valle <francisco.palma.v@universitasxxi.com>
2023-08-22 16:34:38 +0200

    Add custom parallel strategy

--- src/test/java/com/palmadelvalle/utils/Constants.java
+++ src/test/java/com/palmadelvalle/utils/Constants.java
@@ -4,5 +4,6 @@ public class Constants {
     public static final String ZH_HK = "zh-HK";
     public static final String EN = "en";
     public static final String LABEL_MAPPINGS_PATH = "label_mappings.json";
-    public static final int TIMEOUT_IN_SECONDS = 15;
+    public static final int TIMEOUT_IN_SECONDS = 30;
+    public static final int FIXED_PARALLELISM = 5;
 }
